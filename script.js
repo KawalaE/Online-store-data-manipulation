bb5bd592cc37530fe0d6f285b06e77e7b7b37fb7
KawalaE <edyta.wer.kawala@gmail.com>
2023-04-17 13:55:27 +0200

    Display info on the page.

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ let products;
 let carts;
 let users;
 let cartIndex;
+const map = L.map('map').setView([0, 0], 1);
+let categories = document.getElementById('category');
+let cartValueInfo = document.getElementById('cart-val');
+let cartOwnerInfo = document.getElementById('cart-owner');
+let usersInfo = document.getElementById('users');
 
 async function getUsers(){
     const response = await fetch(usersUrl);
@@ -47,30 +52,35 @@ function calcFurthestDistance(){
             }
         }
     }
+    let usersDistHTML = document.createElement('div');
+    usersDistHTML.textContent= `Max distance: ${maxDistance.toFixed(4)}km, Users living furthest (ID's): ${users[maxI].id}, ${users[maxJ].id}`;
+    usersInfo.appendChild(usersDistHTML);
 
     console.log(`Max distance between users: ${maxDistance}km`);
     console.log(`Users living furthest (ID's): ${users[maxI].id}, ${users[maxJ].id}`);
-    drawMap(users[maxI].address.geolocation.lat,users[maxI].address.geolocation.long, 
-        users[maxI].name.firstname,users[maxI].name.lastname, users[maxJ].address.geolocation.lat, users[maxJ].address.geolocation.long, users[maxJ].name.firstname, users[maxJ].name.lastname);
+    drawMarkers(users[maxI].address.geolocation.lat,users[maxI].address.geolocation.long, users[maxI].name.firstname,
+       users[maxI].name.lastname, users[maxJ].address.geolocation.lat, users[maxJ].address.geolocation.long, users[maxJ].name.firstname,users[maxJ].name.lastname);
 }
 function capitalize(word){
     return  word.charAt(0).toUpperCase() + word.slice(1);
 
 }
+async function drawMap(){
 
-function drawMap(lat1, lon1,name1,last1, lat2, lon2, name2, last2){
-    const map = L.map('map').setView([0, 0], 1);
-    
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 20,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
     
+}
+
+function drawMarkers(lat1, lon1, name1, last1, lat2, lon2, name2, last2){
     let clientMarker1 = L.marker([lat1, lon1]).addTo(map);
-    clientMarker1.bindPopup(`<b>${capitalize(name1)} ${capitalize(last1)}<b> <br>Latitude: ${lat1} Longitude: ${lon1}`).openPopup();
     let clientMarker2 = L.marker([lat2, lon2]).addTo(map);
+    clientMarker1.bindPopup(`<b>${capitalize(name1)} ${capitalize(last1)}<b> <br>Latitude: ${lat1} Longitude: ${lon1}`).openPopup();
     clientMarker2.bindPopup(`<b>${capitalize(name2)} ${capitalize(last2)}<b> <br>Latitude: ${lat2} Longitude: ${lon2}`).openPopup();
 }
+
 function categoriesValue(){
     const productCat = new Map;
     for(let i = 0; i< products.length; i++){
@@ -87,7 +97,12 @@ function categoriesValue(){
     }
     for (const[key, value] of productCat){
         console.log(`${key}: ${value}`);
-    } 
+        let newPosition = document.createElement('div');
+        newPosition.textContent = `${key}: ${value.toFixed(2)}$`;
+        categories.appendChild(newPosition);
+    }
+   
+    
 }
 
 function getCartInfo(num){
@@ -127,6 +142,9 @@ function mostExpensiveCart(){
         }
     }
     cartIndex = cartValues.indexOf(maxPrice);
+    let cartValHTML = document.createElement('div');
+    cartValHTML.textContent = `${maxPrice}$`
+    cartValueInfo.appendChild(cartValHTML);
     return `Most expensive cart ${maxPrice}`;
 }
 
@@ -134,11 +152,15 @@ function mostExpensiveCart(){
 function cartOwner(){
     const userName = users[cartIndex].name.firstname;
     const userLastName = users[cartIndex].name.lastname;
+    let cartOwnerHTML = document.createElement('div');
+    cartOwnerHTML.textContent = `${capitalize(userName)} ${capitalize(userLastName)}`;
+    cartOwnerInfo.appendChild(cartOwnerHTML);
     return `${userName} ${userLastName}`
 }
 
-
+drawMap();
 getProducts().then(()=>{
+    categoriesValue();
     getUsers().then(()=>{
         getCarts().then(()=>{
             calcCartValue()
@@ -146,7 +168,7 @@ getProducts().then(()=>{
             console.log(cartOwner());
             console.log(users);
             calcFurthestDistance();
-            categoriesValue();
+            
             
         }); 
     }); 
